54f87d38b349bafe958b8e926b1752f783801301
leleliu008 <leleliu008@gmail.com>
2023-03-13 03:42:49 +0800

    optimized
    Signed-off-by: leleliu008 <leleliu008@gmail.com>

--- src/env.c
+++ src/env.c
@@ -8,9 +8,7 @@
 #include "autotools-setup.h"
 
 #include <zlib.h>
-#include <git2.h>
 #include <yaml.h>
-#include <jansson.h>
 #include <archive.h>
 #include <curl/curlver.h>
 #include <openssl/opensslv.h>
@@ -24,7 +22,6 @@ int autotools_setup_env() {
     //printf("pcre2   : %d.%d\n", PCRE2_MAJOR, PCRE2_MINOR);
     printf("build.libyaml: %s\n", yaml_get_version_string());
     printf("build.libcurl: %s\n", LIBCURL_VERSION);
-    printf("build.libgit2: %s\n", LIBGIT2_VERSION);
 
 //https://www.openssl.org/docs/man3.0/man3/OPENSSL_VERSION_BUILD_METADATA.html
 //https://www.openssl.org/docs/man1.1.1/man3/OPENSSL_VERSION_TEXT.html
@@ -34,7 +31,6 @@ int autotools_setup_env() {
     printf("build.openssl: %s\n", OPENSSL_VERSION_TEXT);
 #endif
 
-    printf("build.jansson: %s\n", JANSSON_VERSION);
     printf("build.archive: %s\n", ARCHIVE_VERSION_ONLY_STRING);
     printf("build.zlib:    %s\n\n", ZLIB_VERSION);
 
